60f120ebe74d180b1ce139a2b4acccb9d95d9d9a
kbroz1 <kmbroz@gmail.com>
2021-05-23 15:55:20 -0600

    completed first part

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Javascript is alive!");
+
+//changes h1 with id "greeting" to different text (from Hello There to Hello World)
+document.getElementById('greeting').innerHTML = 'Hello, World!';
+
